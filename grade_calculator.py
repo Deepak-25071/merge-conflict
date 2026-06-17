cf9da2f1f77741c313d7f597d0e2c43bedee887a
Deepak D C <deepakgowda5757@gmail.com>
2026-06-17 15:56:33 +0530

    Student B: Changed B grade cutoff to 70

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,12 +1,3 @@
-echo "def calculate_grade(marks):
-    if marks >= 90:
-        return 'A'
-    elif marks >= 75:
-        return 'B'
-    elif marks >= 60:
-        return 'C'
-    else:
-        return 'F'
-
-print(calculate_grade(85))" > grade_calculator.py
+elif marks >= 70:
+    return "B"
 
