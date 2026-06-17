ccba59e79abfd68fd6144f0055ee79c938d897f9
Deepak D C <deepakgowda5757@gmail.com>
2026-06-17 15:46:16 +0530

    Base grade calculator

---
+++ grade_calculator.py
@@ -0,0 +1,12 @@
+echo "def calculate_grade(marks):
+    if marks >= 90:
+        return 'A'
+    elif marks >= 75:
+        return 'B'
+    elif marks >= 60:
+        return 'C'
+    else:
+        return 'F'
+
+print(calculate_grade(85))" > grade_calculator.py
+
